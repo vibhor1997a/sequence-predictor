cb9bde7e86b973bf7ce7793e77b6f6272a0b2933
vibhor1997a <vibhor349a@rediffmail.com>
2018-02-14 19:20:52 +0530

    Initial Commit

---
+++ public/javascripts/verify.js
@@ -0,0 +1,46 @@
+let txt = document.getElementById('seq');
+let re = /^\s*\d+(?:(?:\s*,\s*|\s)\d*){11,}\d+\s*$/g;
+let re1 = /(?:\s*,\s*|\s+)/g;
+let btn = document.getElementById('predict');
+// check fuction is invoked every second
+let t = setInterval(check, 1000);
+/**
+ * checks for input validity and enable and disable the btn
+ */
+function check() {
+    let seq = txt.value;
+    let a = seq.match(re);
+    if (a) {
+        btn.disabled = false;
+    }
+    else {
+        btn.disabled = true;
+    }
+}
+//send the GET request to the server on btn click
+btn.addEventListener('click', () => {
+    let s = txt.value;
+    s = s.split(re1).filter(x => (x != '') ? true : false).join(' ');
+    console.log(s);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/predict?seq=' + encodeURIComponent(s));
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+        let DONE = 4; // readyState 4 means the request is done.
+        let OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                let o = JSON.parse(xhr.responseText);
+                let orignal = document.getElementById('orignal');
+                let predicted = document.getElementById('predicted');
+                orignal.innerHTML = o.orignal.join(',&nbsp;') + ',';
+                predicted.innerHTML = '&nbsp;' + o.predicted.join(',&nbsp;');
+                document.getElementById('result').removeAttribute('hidden');
+            }
+            else {
+                console.log('Error: ' + xhr.status); // An error occurred during the request.
+            }
+        }
+    }
+    xhr.send(null);
+});
---
+++ routes/predict.js
@@ -0,0 +1,62 @@
+//timestamp
+let ts = new Date().getTime();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+// Spawed the python script
+let p = spawn('python', ['python/script.py'], { stdio: ['pipe', 'pipe', process.stderr] });
+//piped the stdout to outStream
+console.log(`spawned with pid ${p.pid}`);
+
+let w = fs.createWriteStream('./temp/out_' + ts + '.txt');
+p.stdout.pipe(w);
+
+/* Route '/predict' to predict the seq */
+router.get('/', function (req, res, next) {
+  let seq = req.query.seq;
+  console.log(req.query);
+  managePredictor(seq, (err, o) => {
+    res.send(JSON.stringify(o, null, 4));
+    console.log(JSON.stringify(o, null, 4));
+  });
+});
+
+/**
+ * Manages the predictor in python already running
+ * Passes the Object in the callback if exists or null and error or null
+ * @param {string} seq 
+ * @param {*} callback 
+ */
+function managePredictor(seq, callback) {
+  p.stdin.write(seq + '\n');
+  let re = /{[^{}]*}$/g;
+  let i = 4;
+  let t = setInterval(reader, 1000);
+  function reader() {
+    i--;
+    fs.readFile('./temp/out_' + ts + '.txt', 'utf-8', (err, str) => {
+      str = str.trim();
+      let o = str.match(re);
+      if (err) {
+        callback(err);
+        clearInterval(t);
+      }
+      else {
+        if (o) {
+          callback(null, JSON.parse(o));
+          clearInterval(t);
+        }
+        else {
+          if (i <= 0) {
+            clearInterval(t);
+          }
+        }
+      }
+    });
+  }
+}
+
+module.exports = router;
